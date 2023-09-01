663a67486514401c20007985ccbfd8793606a1ff
pomyslowynick <dark0mlet@gmail.com>
2023-09-01 21:44:18 +0100

    Add a loop to receive packets

--- Makefile
+++ Makefile
@@ -2,7 +2,8 @@ all:
 	cargo build
 	sudo setcap cap_net_admin=eip ./target/debug/rust-practice
 	./target/debug/rust-practice &
-	ip addr add dev tun1 192.168.0.1/32
+	sudo ip addr add dev tun1 192.168.0.1/32
 	echo "setting tun1 to up"
-	ip link set tun1 state up
+	sudo ip link set dev tun1 up
+	sleep 10
 	pkill rust-practice
--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,12 @@ use tun_tap;
 fn main() {
     println!("Hello, world!");
     let iface = tun_tap::Iface::new("tun1", tun_tap::Mode::Tun).expect("Failed to create a tun interface");
+    println!("Hello, world!");
     let mut buffer = vec![0; 1504];
     loop {
-        use std::{thread, time};
-
-        let ten_millis = time::Duration::from_millis(10);
-        let now = time::Instant::now();
-
-        thread::sleep(ten_millis);
         let received = iface.recv(&mut buffer).unwrap();
         println!("{}", received);
-        println!("{:?}", &buffer);
+        println!("{:x?}", &buffer[0:50]);
     }
 }
 
