ef8e40b6eb6ebcdf81661bb1541abd64e21226ec
pomyslowynick <dark0mlet@gmail.com>
2023-09-02 21:46:59 +0100

    Fix the array slice printing

--- Makefile
+++ Makefile
@@ -2,8 +2,8 @@ all:
 	cargo build
 	sudo setcap cap_net_admin=eip ./target/debug/rust-practice
 	./target/debug/rust-practice &
-	sudo ip addr add dev tun1 192.168.0.1/32
+	sudo ip addr add dev tun1 192.168.0.2/26
 	echo "setting tun1 to up"
 	sudo ip link set dev tun1 up
-	sleep 10
+	sleep 60
 	pkill rust-practice
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,15 @@
 use tun_tap;
+use etherparse;
 
 fn main() {
-    println!("Hello, world!");
-    let iface = tun_tap::Iface::new("tun1", tun_tap::Mode::Tun).expect("Failed to create a tun interface");
-    println!("Hello, world!");
+    let iface = tun_tap::Iface::without_packet_info("tun1", tun_tap::Mode::Tun).expect("Failed to create a tun interface");
     let mut buffer = vec![0; 1504];
     loop {
         let received = iface.recv(&mut buffer).unwrap();
-        println!("{}", received);
-        println!("{:x?}", &buffer[0:50]);
+        println!("{:x?}", &buffer[0..50]);
     }
 }
 
-fn connnect() {
+fn connect() {
     unimplemented!()
 }
