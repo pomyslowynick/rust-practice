3d397a87a4e55f937cf1effe4fb12c76c117376d
pomyslowynick <dark0mlet@gmail.com>
2023-09-03 22:14:37 +0100

    Add parsing method
    Signed-off-by: pomyslowynick <dark0mlet@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,20 @@ fn main() {
     let mut buffer = vec![0; 1504];
     loop {
         let received = iface.recv(&mut buffer).unwrap();
-        println!("{:x?}", &buffer[0..50]);
+        println!("{:x?}", &buffer);
+
+
+        //let tcp_header = etherparse::TcpHeaderSlice::from_slice(&buffer).unwrap();
+        match etherparse::PacketHeaders::from_ethernet_slice(&buffer) {
+            Err(value) => println!("{:?}", value),
+            Ok(value) => {
+                println!("link: {:?}", value.link);
+                println!("vlan: {:?}", value.vlan);
+                println!("ip: {:?}", value.ip);
+                println!("transport: {:?}", value.transport);
+            }
+
+        }
     }
 }
 
