5d09c879798dc6e38b9b4148d153b1144c5af17e
pomyslowynick <dark0mlet@gmail.com>
2023-09-06 08:52:21 +0100

    Remove redundant prints
    Signed-off-by: pomyslowynick <dark0mlet@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -6,29 +6,19 @@ fn main() {
     let mut buffer = vec![0; 1500];
     loop {
         let received = iface.recv(&mut buffer).unwrap();
-        println!("{:x?}", &buffer);
-
-
-        //let tcp_header = etherparse::TcpHeaderSlice::from_slice(&buffer).unwrap();
         match etherparse::Ipv4HeaderSlice::from_slice(&buffer) {
-            Err(value) => println!("{:?}", value),
+            Err(value) => println!("Error parsing IPv4 {:?}", value),
             Ok(value) => {
-                println!("Ipv4 Header: {:?}", value);
-                println!("Ipv4 Total Length: {:?}", value.total_len());
-                println!("Ipv4 source: {:?}", value.source());
-                println!("Ipv4 protocol: {:?}", value.protocol());
-                println!("Ipv4Header 2 {:?}", value.to_header());
-                println!("Ipv4Header IHL {:?}", value.ihl());
-
-                match etherparse::TcpHeaderSlice::from_slice(&buffer[(value.ihl() * 4) as usize..]) {
-                    Err(tcpheader) => println!("{:?}", tcpheader),
-                    Ok(tcpheader) => println!("{:?}", tcpheader.to_header())
+                if value.protocol() == 6 {
+                    match etherparse::TcpHeaderSlice::from_slice(&buffer[(value.ihl() * 4) as usize..]) {
+                        Err(tcpheader) => println!("{:?}", tcpheader),
+                        Ok(tcpheader) => {
+                            println!("{:?}", tcpheader.to_header());
+                            connect(tcpheader);
+                        }
+                    }
                 }
             }
         }
     }
-}
-
-fn connect() {
-    unimplemented!()
 }
