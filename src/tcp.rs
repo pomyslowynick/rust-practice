d088d59f197ebdaa82be63d2304584b5ff593402
pomyslowynick <dark0mlet@gmail.com>
2023-09-08 08:49:37 +0100

    Correctly write a TCP packet to a buffer
    Signed-off-by: pomyslowynick <dark0mlet@gmail.com>

--- src/tcp.rs
+++ src/tcp.rs
@@ -24,12 +24,16 @@ impl <'a> Connection<'a> {
         let mut buffer = vec![0; 1500];
         let ip_packet = Ipv4Header::new(0, 64, 6, self.ip_header.destination(), self.ip_header.source());
 
-        let writing_result = ip_packet.write(&mut buffer).unwrap();
 
         let tcp_packet = TcpHeader::new(self.tcp_header.destination_port(), self.tcp_header.destination_port(), 0, 1 );
-        let tcp_result = tcp_packet.write(&mut buffer[ip_packet.header_len()..]).unwrap();
 
+        use std::io::Write;
+        let mut unwritten = &mut buffer[..];
+        let writing_result = ip_packet.write(&mut unwritten).unwrap();
+        let tcp_result = tcp_packet.write(&mut unwritten).unwrap();
+
+        let unwritten = unwritten.len();
         println!("{:?}", buffer);
-        nic.send(&buffer);
+        nic.send(&buffer[..buffer.len()-unwritten]);
     }
 }
